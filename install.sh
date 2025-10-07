ed2a7ddca9d4ef5528fdc4d5ca2b1791fb53eed5
dbornstein <dave@bornsteins.com>
2025-10-07 16:47:26 -0400

    update

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bim/bash
 
+# cd /tmp  # Or clone your repo here
+# git clone https://github.com/dbornstein/noc-control.git noc-agent-src
+
 sudo adduser --system --group --no-create-home noc-agent
 
 sudo mkdir -p /opt/noc-agent
 sudo chown noc-agent:noc-agent /opt/noc-agent
-sudo -u noc-agent python3.13 -m venv /opt/noc-agent/venv
+sudo -u noc-agent python3 -m venv /opt/noc-agent/venv
 
-sudo -u noc-agent /opt/noc-agent/venv/bin/activate
-cd /tmp  # Or clone your repo here
-git clone <your-repo-url> noc-agent-src
-cd noc-agent-src
+cd /tmp/noc-agent-src
 pip install -e .  # Editable install for easy updates
 deactivate
 
